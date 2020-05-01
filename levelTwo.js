8d4768afd6304149bd34c4edabca025487970dcb
Javier García <javigdeveloper@gmail.com>
2020-04-30 22:25:08 -0500

    functionality for level two

--- levelTwo.js
+++ levelTwo.js
@@ -1,35 +1,50 @@
-let image = document.getElementById("container");
-
-function changeItem() {
-  let newDiv = document.createElement("div");
-  newDiv.setAttribute("class", "second");
-  image.appendChild(newDiv);
-  // newDiv.setAttribute("class", "first");
-  // image.insertBefore(newDiv, document.getElementById("second"))
+// function is called at the for loop to fill in all the possible answers
+function generateNums(arr) {
+  let innerArr = arr.splice(Math.floor(Math.random() * arr.length), 1);
+  let n = innerArr[0][0];
+  let d = innerArr[0][1];
+  return [n, d];
 }
 
-
-// let's change this function to pass an array with ALL the original posibilities and remove it
-// from the array once used to avoid repeats
-function generateNums() {
-  let d = Math.floor(Math.random() * 5) + 2;
-  let n = Math.floor(Math.random() * d);
-  if(n === 0) {
-    n = 1;
-  }
-  return [n, d]
-}
-
+let arrays = [[1, 2], [1, 3], [2, 3], [1, 4], [2, 4], [3, 4], [1, 5], [2, 5], [3, 5],
+[4, 5], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6]];
 
 let numerator = document.getElementsByClassName("numerator");
 let denominator = document.getElementsByClassName("denominator");
 
 
 for(let i = 0; i < numerator.length; i++) {
-  let runOnce = generateNums()
+  console.log(arrays);
+  let runOnce = generateNums(arrays);
   numerator[i].innerHTML = runOnce[0];
   denominator[i].innerHTML = runOnce[1];
 }
 
+// choosing one of the answers to match with the image
+let random = Math.floor(Math.random() * 4)
+let upper = numerator[random].innerHTML;
+let bottom =  denominator[random].innerHTML;
+console.log(upper);
+console.log(bottom);
 
-// console.log(numerator);
+let image = document.getElementById("container");
+
+if(upper > 1) {
+  for(let i = 1; i < upper; i++) {
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "second");
+    image.appendChild(newDiv);
+  }
+}
+
+upper = parseInt(upper)
+bottom = parseInt(bottom)
+
+if(bottom > upper + 1) {
+  let times = bottom - (upper + 1);
+  for(let j = 0; j < times; j++) {
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "first");
+    image.insertBefore(newDiv, document.getElementById("second"))
+  }
+}
