fb97518718f5bb2848f97a4f5c9cd87b3feec9cd
Javier García <javigdeveloper@gmail.com>
2020-05-02 21:13:15 -0500

    level two is level one now

---
+++ levelFour.js
@@ -0,0 +1,17 @@
+let classes = ["three", "four", "five", "six"];
+let classesForDivs = ["third", "fourth", "fifth", "sixth"];
+let image = document.getElementById("figure");
+console.log("this is " + image.className)
+
+function changeItem() {
+  let i = classes[0];
+  let j = classesForDivs[0];
+  if(classes.length > 0) {
+    let newDiv = document.createElement("div");
+    image.appendChild(newDiv);
+    newDiv.setAttribute("class", j)
+    image.setAttribute("class", i)
+    classes.shift();
+    classesForDivs.shift();
+  }
+}
--- levelOne.js
+++ levelOne.js
@@ -1,17 +1,51 @@
-let classes = ["three", "four", "five", "six"];
-let classesForDivs = ["third", "fourth", "fifth", "sixth"];
-let image = document.getElementById("figure");
-console.log("this is " + image.className)
+// function is called at the for loop to fill in all the possible answers
+function generateNums(arr) {
+  let innerArr = arr.splice(Math.floor(Math.random() * arr.length), 1);
+  let n = innerArr[0][0];
+  let d = innerArr[0][1];
+  return [n, d];
+}
 
-function changeItem() {
-  let i = classes[0];
-  let j = classesForDivs[0];
-  if(classes.length > 0) {
+let arrays = [[1, 2], [1, 3], [2, 3], [1, 4], [2, 4], [3, 4], [1, 5], [2, 5], [3, 5],
+[4, 5], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6]];
+
+let numerator = document.getElementsByClassName("numerator");
+let denominator = document.getElementsByClassName("denominator");
+
+
+for(let i = 0; i < numerator.length; i++) {
+  console.log(arrays);
+  let runOnce = generateNums(arrays);
+  numerator[i].innerHTML = runOnce[0];
+  denominator[i].innerHTML = runOnce[1];
+}
+
+// choosing one of the answers to match with the image
+let random = Math.floor(Math.random() * 4)
+let upper = numerator[random].innerHTML;
+let bottom =  denominator[random].innerHTML;
+console.log(upper);
+console.log(bottom);
+
+let image = document.getElementById("container");
+upper = parseInt(upper)
+bottom = parseInt(bottom)
+
+// adding coloured fractions (right of the image)
+if(upper > 1) {
+  for(let i = 1; i < upper; i++) {
     let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "second");
     image.appendChild(newDiv);
-    newDiv.setAttribute("class", j)
-    image.setAttribute("class", i)
-    classes.shift();
-    classesForDivs.shift();
+  }
+}
+
+// adding blank fractions (left of the image)
+if(bottom > upper + 1) {
+  let times = bottom - (upper + 1);
+  for(let j = 0; j < times; j++) {
+    let newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "first");
+    image.insertBefore(newDiv, document.getElementById("second"))
   }
 }
