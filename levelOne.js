5f88b00b45aa474d9bef93254514eebdaa46ba9a
Javier García <javigdeveloper@gmail.com>
2020-05-02 23:57:48 -0500

    submit button first functionality working

--- levelOne.js
+++ levelOne.js
@@ -1,51 +1,45 @@
-// function is called at the for loop to fill in all the possible answers
-function generateNums(arr) {
-  let innerArr = arr.splice(Math.floor(Math.random() * arr.length), 1);
-  let n = innerArr[0][0];
-  let d = innerArr[0][1];
-  return [n, d];
-}
-
-let arrays = [[1, 2], [1, 3], [2, 3], [1, 4], [2, 4], [3, 4], [1, 5], [2, 5], [3, 5],
-[4, 5], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6]];
-
-let numerator = document.getElementsByClassName("numerator");
-let denominator = document.getElementsByClassName("denominator");
-
-
-for(let i = 0; i < numerator.length; i++) {
-  console.log(arrays);
-  let runOnce = generateNums(arrays);
-  numerator[i].innerHTML = runOnce[0];
-  denominator[i].innerHTML = runOnce[1];
-}
-
-// choosing one of the answers to match with the image
-let random = Math.floor(Math.random() * 4)
-let upper = numerator[random].innerHTML;
-let bottom =  denominator[random].innerHTML;
-console.log(upper);
-console.log(bottom);
-
-let image = document.getElementById("container");
-upper = parseInt(upper)
-bottom = parseInt(bottom)
-
-// adding coloured fractions (right of the image)
-if(upper > 1) {
-  for(let i = 1; i < upper; i++) {
+function generateNums() {
+  // clearing the image of previous times
+  let leftFraction = document.getElementsByClassName("first");
+  let rightFraction = document.getElementsByClassName("second");
+  while(leftFraction.length > 0) {
+    leftFraction[0].remove();
+  }
+  while(rightFraction.length > 0) {
+    rightFraction[0].remove();
+  }
+  // defining array with all possible answers
+  let arr = [[1, 2], [1, 3], [2, 3], [1, 4], [2, 4], [3, 4], [1, 5], [2, 5], [3, 5],
+  [4, 5], [1, 6], [2, 6], [3, 6], [4, 6], [5, 6]];
+  // getting html to put the answers in
+  let numerator = document.getElementsByClassName("numerator");
+  let denominator = document.getElementsByClassName("denominator");
+  // filling in all the answers
+  for(let j = 0; j < numerator.length; j++) {
+    let innerArr = arr.splice(Math.floor(Math.random() * arr.length), 1);
+    numerator[j].innerHTML = innerArr[0][0];
+    denominator[j].innerHTML = innerArr[0][1];
+  }  
+  // choosing one of the answers to match with the image
+  let random = Math.floor(Math.random() * 4)
+  let upper = numerator[random].innerHTML;
+  let bottom =  denominator[random].innerHTML;
+  // these numbers can't be strings so that they can be manipulated inside the for loop
+  upper = parseInt(upper)
+  bottom = parseInt(bottom)
+  // adding coloured fractions (right of the image)
+  let image = document.getElementById("container");
+  for(let i = 0; i < upper; i++) {
     let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "second");
     image.appendChild(newDiv);
   }
-}
-
-// adding blank fractions (left of the image)
-if(bottom > upper + 1) {
-  let times = bottom - (upper + 1);
+  // adding blank fractions (left of the image)
+  let times = bottom - upper;
   for(let j = 0; j < times; j++) {
     let newDiv = document.createElement("div");
     newDiv.setAttribute("class", "first");
     image.insertBefore(newDiv, document.getElementById("second"))
   }
 }
+generateNums()
